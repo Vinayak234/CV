a765af7a36833c01ff58713c705ba4b8dee8a2ec
Patel Vinayak <patel.vinayak234@gmail.com>
2020-01-10 12:44:32 -0500

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+jQuery(function ($) {
+    'use strict';
+    (function () {
+        $('#preloader').delay(200).fadeOut('slow');
+    }());
+    $('.left-col-block, .right-col-block').theiaStickySidebar();
+});
